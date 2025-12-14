42923ca50847c9a49af7af112d7d3249c824a31f
jalel <ghabri.djalel@gmail.com>
2025-12-14 21:22:16 +0000

    Add Color Picker tool
    - Visual color picker with HTML5 color input
    - Convert between HEX, RGB, HSL, and CMYK formats
    - Color harmony: complementary, triadic, analogous colors
    - Generate shades and tints
    - Display color info: brightness, luminance, suggested text color
    - Quick-select common colors palette
    - Copy color values to clipboard
    - Random color generator
    - Client-side only implementation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -100,6 +100,11 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"/>
                         </svg>
                         @break
+                    @case('color')
+                        <svg class="w-6 h-6 text-indigo-600 dark:text-indigo-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"/>
+                        </svg>
+                        @break
                 @endswitch
             </div>
             <div class="flex-1 min-w-0">
